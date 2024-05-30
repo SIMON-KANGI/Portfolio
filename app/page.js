895817393b89d6024d417f86b7df065d3e546357
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-30 21:51:20 +0300

    Home

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       <section id="display" className='grid lg:grid-cols-2 grid-cols-1 w-full my-6 h-screen'>
         <div id="display-1" data-aos="fade-right"  className=" justify-center flex-col mt-10 overflow-hidden lg:order-1 order-2 w-fit">
         <div className='block text-center lg:text-left  left-0'>
-        <h1 className="text-center lg:text-left text-xl">Hello I'm</h1>
+        <h1 className="text-center lg:text-left text-xl">Hello I &apos; m</h1>
           <h1 className="lg:text-5xl text-3xl font-extrabold pt-2"><span className='text-rose-600'>Simon </span>Mwangi</h1>
           </div>
           <p className="lg:text-4xl text-center lg:text-left  text-xl py-6 text-primary font-semibold text-rose-500">
@@ -35,8 +35,8 @@ export default function Home() {
               robust mobile, desktop and web application
           </article>
           <div className='py-3 lg:mx-0  my-4'>
-            <button className='text-center lg:w-fit w-full my-2 hover:bg-black hover:border hover:border-rose-600 bg-rose-600 lg:px-8 py-3 rounded-md font-bold text-slate-100'>View Resume</button>
-            <button className='text-center lg:w-fit w-full my-2 bg-black border border-rose-600 hover:bg-rose-600 lg:px-8 py-3 rounded-md font-bold text-slate-100'>Contact</button>
+            <button className='text-center lg:w-fit w-full my-2 lg:mx-3 hover:bg-black hover:border hover:border-rose-600 bg-rose-600 lg:px-8 py-3 rounded-md font-bold text-slate-100'>View Resume</button>
+            <button className='text-center lg:w-fit w-full my-2 bg-black border border-rose-600 hover:bg-rose-600 lg:px-10 py-3 rounded-md font-bold text-slate-100'>Contact</button>
           </div>
           <div>
             <Socials/>
@@ -49,6 +49,7 @@ export default function Home() {
           width="600"
           height="600"
           className='rounded-full'
+          alt="ndovu"
          
          />
         </div>
@@ -56,7 +57,7 @@ export default function Home() {
       <section id='about' className='lg:mx-0 mx-4' data-aos="fade-up">
       <About/>
       </section>
-      <section id="services" className='text-center mt-3' data-aos="fade-up">
+      <section id="services" className='text-center lg:mt-3 my-8' data-aos="fade-up">
         <h1 className='text-center text-3xl font-medium'>Here are some of the current<br/> <span className='text-rose-600'>services</span> I can offer</h1>
 
         <Services/>
