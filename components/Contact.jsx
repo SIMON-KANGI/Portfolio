6645893f86e1b7fef313b042c48bc0513c281da1
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-30 17:17:16 +0300

    contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,9 +1,39 @@
+import { Card, CardBody } from '@chakra-ui/react'
 import React from 'react'
-
+import { CircleChevronRight } from 'lucide-react'
+import Form from './Form'
 function Contact() {
   return (
-    <div>
-      
+    <div className='lg:grid grid-cols-2'>
+      <Card bg="stone.900" className='lg:mx-0 mx-6'>
+      <h1 className='text-xl'>Reach Out</h1>
+  <p className='text-4xl text-slate-200'>I am fully Available for a <span className='text-rose-600'> Job</span> or<br/> 
+  <span className='text-rose-600'> internship </span>Opportunities</p>
+  <p className='text-slate-200 text-xl my-4'>You can reach me via Email or Phone call</p>
+  <ul>
+    <li className='text-xl text-slate-200 my-4 flex'>
+    <CircleChevronRight className='text-rose-600' />
+    Software Development
+    </li>
+    <li className='text-xl text-slate-200 my-4 flex'>
+    <CircleChevronRight className='text-rose-600' />
+    Web Design
+    </li>
+    <li className='text-xl text-slate-200 my-4 flex'>
+    <CircleChevronRight className='text-rose-600' />
+    Internship
+    </li>
+    <li className='text-xl text-slate-200 my-4 flex'>
+    <CircleChevronRight className='text-rose-600' />
+    FullStack Developer
+    </li>
+  </ul>
+      </Card>
+      <Card bg="stone.900">
+        <CardBody>
+          <Form/>
+        </CardBody>
+      </Card>
     </div>
   )
 }
