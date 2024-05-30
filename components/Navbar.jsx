98b96d68347eeb3494b2a976c563aed970ebdc50
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-30 17:18:17 +0300

    NAVBAR

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import SideBar from './SideBar'
 
 function Navbar() {
   return (
     <div>
-    <header className='flex w-full p-6  top-0 z-10 justify-around'>
+    <header className='flex w-full p-6  top-0 z-10 lg:justify-around justify-between'>
     <h1 className='text-slate-100 font-bold text-2xl font-serif'><span className='text-rose-600'>Simon </span>Mwangi</h1>
-        <nav className='text-center'>
+        <nav className='text-center lg:flex hidden'>
             <ul className='flex text-white font-bold'>
                 <li className='mx-3'><a href="/">Home</a></li>
                 <li className='mx-3'><a href="#about">About</a></li>
@@ -15,9 +16,10 @@ function Navbar() {
                 <li className='mx-3'><a href="#ontact">Contact</a></li>
             </ul>
         </nav>
+        <div className='lg:hidden flex'><SideBar/></div>
     </header>
-      
-    </div>
+     
+    </div> 
   )
 }
 
