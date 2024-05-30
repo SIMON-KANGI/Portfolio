dd61233f7a3c7ea52deb29a6d1df3c790f3ff3b3
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-30 21:51:36 +0300

    NavBar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+'use client'
+
+import React,{useEffect} from 'react'
 import SideBar from './SideBar'
+import Link from 'next/link';
 
 function Navbar() {
+  useEffect(() => {
+    const handleScroll = () => {
+        const navbar = document.querySelector(".header");
+        if (navbar) {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            if (scrollTop > 0) {
+                navbar.classList.add("navbar-glassmorphism");
+                navbar.classList.add("shadow-md");
+            } else {
+                navbar.classList.remove("navbar-glassmorphism");
+                navbar.classList.remove("shadow-md");
+            }
+        }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+}, []);
+
   return (
-    <div>
-    <header className='flex w-full p-6  top-0 z-10 lg:justify-around justify-between'>
-    <h1 className='text-slate-100 font-bold text-2xl font-serif'><span className='text-rose-600'>Simon </span>Mwangi</h1>
-        <nav className='text-center lg:flex hidden'>
+    
+    <header className='header flex w-full p-6 sticky  top-0 z-10 lg:justify-around justify-between' id="header">
+    <Link href="/" className='text-slate-100 font-bold text-2xl font-serif'><span className='text-rose-600'>Simon </span>Mwangi</Link>
+        <nav className='text-center lg:flex hidden' >
             <ul className='flex text-white font-bold'>
                 <li className='mx-3'><a href="/">Home</a></li>
                 <li className='mx-3'><a href="#about">About</a></li>
@@ -19,7 +41,7 @@ function Navbar() {
         <div className='lg:hidden flex'><SideBar/></div>
     </header>
      
-    </div> 
+    
   )
 }
 
