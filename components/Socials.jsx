77fa6b7341ef644be979d3bb5681520d69170f6b
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-30 17:20:03 +0300

    added theme

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -6,35 +6,35 @@ import { Card, CardBody, Tooltip } from '@chakra-ui/react';
 function Socials() {
   return (
     <div className='flex'>
-    <Card className='bg-gray-800 hover:bg-rose-600 hover:text-slate-200 text-rose-600 p-2 mx-2 rounded-md'>
+    <Card color="rose.600" bg="stone.800" className='bg-stone-800 hover:bg-rose-600 hover:text-slate-200 text-rose-600 p-2 mx-2 rounded-md'>
     <CardBody>
-    <Tooltip label="Github" bg='gray.300' color="white" border="2px">
+    <Tooltip bg="stone.800" label="Github" color="white" border="2px">
          <Github />
     </Tooltip>
        
     </CardBody>
     </Card>
-    <Card className='bg-gray-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
+    <Card color="rose.600" bg="stone.800" className='bg-stone-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
     <CardBody>
-    <Tooltip label="linkedin" bg='gray.300' color="white" border="2px">
+    <Tooltip bg="stone.800" label="linkedin"  color="white" border="2px">
          <Linkedin />
     </Tooltip>
         
     </CardBody>
    
     </Card>
-    <Card className='bg-gray-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
+    <Card color="rose.600" bg="stone.800" className='bg-stone-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
     <CardBody>
-    <Tooltip label="X" bg='gray.300' color="white" border="2px">
+    <Tooltip bg="stone.800" label="X"  color="white" border="2px">
          <X /> 
     </Tooltip>
       
     </CardBody>
     
     </Card>
-    <Card className='bg-gray-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
+    <Card color="rose.600" bg="stone.800" className='bg-stone-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
     <CardBody>
-    <Tooltip label="Mail" bg='gray.300' color="white" border="2px">
+    <Tooltip bg="stone.800" label="Mail"  color="white" border="2px">
            <Mail/> 
     </Tooltip>
     
