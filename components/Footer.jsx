8ccc756ae1eb4d2060ac1605dfa92bf43f527ba2
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-31 08:29:58 +0300

    added footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     
     <footer className='flex lg:w-screen mt-8 p-6 bg-stone-800 top-0 z-10 justify-around'>
-    <h1 className='text-slate-100 font-bold lg:text-2xl text-xl font-serif'>Copyright &copy;2024 <span className='text-rose-600 text-base'>Simon Mwangi </span>
+    <h1 className='text-slate-100 font-bold lg:text-2xl  font-serif'>Copyright &copy;2024 <span className='text-rose-600 text-base'>Simon Mwangi </span>
     All Rights Reserved
     </h1>
         <nav className='text-center lg:flex bg-none hidden'>
