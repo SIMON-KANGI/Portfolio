3e476f28eee78eae320c93f9a9d1d98b0cfd861c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-31 13:37:31 +0300

    chakra

--- components/chakra/chakra.jsx
+++ components/chakra/chakra.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { ChakraProvider } from "@chakra-ui/react"
 import appTheme from "../theme/Theme"
+import Mouse from "../theme/Mouse"
 function Chakra({children}){
     return <ChakraProvider theme={appTheme}>{children}</ChakraProvider>
 
