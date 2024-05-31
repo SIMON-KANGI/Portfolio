0e7916e72dbc8e400fa94d5b54ecefc245e4d837
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-31 08:31:12 +0300

    mobile view

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,49 +2,48 @@
 
 import React from 'react'
 import { Github, Linkedin, Mail, X } from 'lucide-react';
-import { Card, CardBody, Tooltip } from '@chakra-ui/react';
+import { Box, Card, CardBody, Tooltip } from '@chakra-ui/react';
 import Link from 'next/link';
 function Socials() {
   return (
     <div className='flex'>
-    <Card color="rose.600" bg="stone.800" className='bg-stone-800 hover:bg-rose-600 hover:text-slate-200 text-rose-600 p-2 mx-2 rounded-md'>
-    <CardBody>
+   
+   
     <Tooltip bg="stone.800" label="Github" color="white" border="2px">
-    <Link href='https://github.com/SIMON-KANGI'>
+    <Link href='https://github.com/SIMON-KANGI' color="rose.600" bg="stone.800" className='bg-stone-800 hover:bg-rose-600 flex flex-col justify-center items-center hover:text-slate-200 text-rose-600 p-3  mx-2 rounded-md'>
        <Github />
     </Link>
        
     </Tooltip>
        
-    </CardBody>
-    </Card>
-    <Card color="rose.600" bg="stone.800" className='bg-stone-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
-    <CardBody>
-    <Tooltip bg="stone.800" label="linkedin"  color="white" border="2px">
-         <Linkedin />
+   <Tooltip bg="stone.800" label="linkedin"  color="white" border="2px">
+    <Link href="/" color="rose.600" bg="stone.800" className='bg-stone-800 flex flex-col justify-center items-center  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-3  mx-2 rounded-md'>
+      <Linkedin />
+    </Link>
+         
     </Tooltip>
         
-    </CardBody>
-   
-    </Card>
-    <Card color="rose.600" bg="stone.800" className='bg-stone-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
-    <CardBody>
-    <Tooltip bg="stone.800" label="X"  color="white" border="2px">
-         <X /> 
+   <Tooltip bg="stone.800" label="X"  color="white" border="2px">
+    <Link href="/" color="rose.600" bg="stone.800" className='bg-stone-800 flex flex-col justify-center items-center  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-3  mx-2 rounded-md'>
+        <X /> 
+    </Link>
+       
     </Tooltip>
       
-    </CardBody>
+   
     
-    </Card>
-    <Card color="rose.600" bg="stone.800" className='bg-stone-800  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-2 mx-2 rounded-md'>
-    <CardBody>
+ 
+   
     <Tooltip bg="stone.800" label="Mail"  color="white" border="2px">
-           <Mail/> 
+    <Link href="/" color="rose.600" bg="stone.800" className='bg-stone-800 flex flex-col justify-center items-center  hover:bg-rose-600 hover:text-slate-200  text-rose-600 p-3  mx-2 rounded-md'>
+      <Mail/> 
+    </Link>
+           
     </Tooltip>
     
-    </CardBody>
+ 
     
-    </Card>
+   
       
     </div>
   )
