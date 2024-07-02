e37cbb84bfc2e946311d50f565424f31e1b8c7ec
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-02 17:22:40 +0300

    added repayapp

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -85,6 +85,38 @@ function Projects() {
           </Tooltip>
           
         </div>
+        
+      </CardBody>
+      
+      </Card>
+      <Card bg="stone.900" className='my-16' >
+      <CardBody className='lg:flex'>
+     
+        <div className='mx-8 w-fit order-1 lg:order-2'>
+        <span className='text-rose-600'>Web App</span>
+          <h1 className='lg:text-4xl text-2xl my-3'>REPAY</h1>
+          <article className='text-slate-200 text-xl  leading-loose'>
+          The Account Management App is a comprehensive solution designed to streamline <br/>and enhance the management of 
+          personal and business accounts. This application <br/>provides users with a centralized platform to track, 
+          manage, and analyze their financial <br/>activities with ease and efficiency.
+          </article>
+          <Tooltip label="live demo" color="white" border=" solid 1px red" paddingX={"5px"}>
+            <Link target="_blank" href="https://repayapp.netlify.app/" className='rounded-full border my-8 border-rose-600 
+           text-rose-600 font-bold hover:bg-rose-600 hover:text-slate-200 w-14 h-14 justify-center flex items-center'>
+          <ArrowUpRight />
+          </Link>
+          </Tooltip>
+          
+        </div> 
+        <div className='border-2 border-stone-800 p-8 hover:border-rose-600 lg:order-2 order-1 rounded-md'>
+        <Image
+        src="/repay.png"
+        width="500"
+        height="500"
+        alt="techverse"
+      />
+      </div>
+        
       </CardBody>
       
       </Card>
