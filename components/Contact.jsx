e084f45cb9954a31734671f06a53a88397120623
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-27 11:05:56 +0300

    contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -14,7 +14,7 @@ function Contact() {
   }, []);
   return (
     <div className='lg:grid grid-cols-2'>
-      <Card bg="stone.900" className='lg:mx-0 mx-6' data-aos="fade-right">
+      <Card bg="stone.900" className='lg:mx-0 mx-6'>
       <h1 className='text-xl'>Reach Out</h1>
   <p className='lg:text-4xl text-2xl text-slate-200'>I am fully Available for a <span className='text-rose-600'> Job</span> or<br/> 
   <span className='text-rose-600'> internship </span>Opportunities</p>
