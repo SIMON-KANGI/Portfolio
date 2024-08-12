98b007ffd756ae7d18ef5d7e070d48ca9163be85
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-12 14:06:06 +0300

    services

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,10 +1,20 @@
 import { Card, CardBody, CardHeader } from '@chakra-ui/react'
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Globe,CodeXml, Bug } from 'lucide-react'
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 function Services() {
+  useEffect(() => {
+    AOS.init({
+      duration: 200,
+      easing: 'ease-in-out',
+      once: true
+    });
+  }, []);
   return (
     <div className='grid lg:grid-cols-3 grid-cols-1 lg:mx-0 mx-3 mt-3'>
-      <Card color="rose.600" bg="stone.800" className='bg-stone-900 flex flex-col my-2 justify-center items-center  hover:border hover:border-rose-600  text-rose-600 p-4 mx-2 rounded-md'>
+      <Card color="rose.600" bg="stone.800" className='bg-stone-900 flex flex-col my-2 justify-center items-center 
+       hover:border hover:border-rose-600  text-rose-600 p-4 mx-2 rounded-md' data-aos="flip-right">
       <CardBody>
         <div className='flex justify-center items-center'>
           <Globe />   
@@ -15,7 +25,7 @@ function Services() {
         <p className='text-slate-100 text-xl'>Design responsive webpages for any use. ie Eccomerce Websites, Blogs etc</p>
       </CardBody>
     </Card>
-      <Card color="rose.600" bg="stone.800" className='bg-stone-900 flex  my-2 flex-col justify-center items-center hover:border hover:border-rose-600  text-rose-600 p-2 mx-2 rounded-md'>
+      <Card color="rose.600" data-aos="flip-right" bg="stone.800" className='bg-stone-900 flex  my-2 flex-col justify-center items-center hover:border hover:border-rose-600  text-rose-600 p-2 mx-2 rounded-md'>
         <CardBody>
         <CardHeader className='flex justify-center items-center'>
          <CodeXml />
@@ -25,7 +35,7 @@ function Services() {
         <p className='text-slate-100 text-xl leading-loose'>Design highly scallable restfull APIs and test API endpoints</p>
         </CardBody>
       </Card>
-      <Card color="rose.600" bg="stone.800" className='bg-stone-900 flex my-2 flex-col justify-center items-center hover:border hover:border-rose-600  text-rose-600 p-2 mx-2 rounded-md'>
+      <Card color="rose.600" data-aos="flip-right" bg="stone.800" className='bg-stone-900 flex my-2 flex-col justify-center items-center hover:border hover:border-rose-600  text-rose-600 p-2 mx-2 rounded-md'>
         <CardBody>
         <CardHeader className='flex justify-center items-center'>
         <Bug />
