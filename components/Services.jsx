1f05cf9309dad9dc840e60e8427d59fb44ce6428
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-12 13:00:10 +0300

    services

--- components/Services.jsx
+++ components/Services.jsx
@@ -4,12 +4,12 @@ import { Globe,CodeXml, Bug } from 'lucide-react'
 function Services() {
   return (
     <div className='grid lg:grid-cols-3 grid-cols-1 lg:mx-0 mx-3 mt-3'>
-      <Card color="rose.600" bg="stone.800" className='bg-stone-900 flex flex-col my-2 justify-center items-center hover:border hover:border-rose-600  text-rose-600 p-4 mx-2 rounded-md'>
+      <Card color="rose.600" bg="stone.800" className='bg-stone-900 flex flex-col my-2 justify-center items-center  hover:border hover:border-rose-600  text-rose-600 p-4 mx-2 rounded-md'>
       <CardBody>
         <div className='flex justify-center items-center'>
           <Globe />   
         </div>
-        <h1 className='text-slate-100 text-center text-xl py-2'>
+        <h1 className=' text-center text-xl py-2'>
           Web Design
         </h1>
         <p className='text-slate-100 text-xl'>Design responsive webpages for any use. ie Eccomerce Websites, Blogs etc</p>
