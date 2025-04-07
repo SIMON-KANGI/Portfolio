335b731b354f94176017126761a698efee431c4c
SIMON-KANGI <simonmwangikangi@gmail.com>
2025-04-07 20:41:27 +0300

    updated projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -19,7 +19,7 @@ function Projects() {
       <CardBody className='lg:flex'>
       <div className='border-2 border-stone-800 p-8 hover:border-rose-600 rounded-md'>
         <Image
-        src="/petwill.png"
+        src="/ibima.png"
         width="500"
         height="500"
         alt="petWill"
@@ -27,13 +27,12 @@ function Projects() {
       </div>
         <div className='mx-8 w-fit'>
         <span className='text-rose-600'>Web App</span>
-          <h1 className='lg:text-4xl text-2xl my-3'>The Petwill Company <br/>Dashboard</h1>
+          <h1 className='lg:text-4xl text-2xl my-3'>IBIMA Company <br/>Dashboard</h1>
           <article className='text-slate-200 text-xl leading-loose'>
-            Collaborated with my group of aspiring developers and came up with a<br/> company dashboard for a 
-            construction company. A web application <br/>for the Petwill Company to manage their business
+            Developed an admin dashboard for Ibima insurance company<br/> that helps the company manage their sales and transactions<br/>
           </article>
           <Tooltip label="live demo" color="white" border=" solid 1px red" paddingX={"5px"}>
-            <Link target="_blank" href="https://petwill.netlify.app/" className='rounded-full border my-8 border-rose-600 
+            <Link target="_blank" href="https://admin.demo.ibima.co.ke/login" className='rounded-full border my-8 border-rose-600 
            text-rose-600 font-bold hover:bg-rose-600 hover:text-slate-200 w-14 h-14 justify-center flex items-center'>
           <ArrowUpRight />
           </Link>
@@ -47,7 +46,7 @@ function Projects() {
       <CardBody className='lg:flex flex-col lg:flex-row'>
         <div className='border-2 border-stone-800 p-8 hover:border-rose-600 rounded-md order-1 lg:order-2'>
           <Image
-            src="/fresh.png"
+            src="/clientibima.png"
             width="500"
             height="500"
             alt="Fresh Picks Groceries"
@@ -55,13 +54,13 @@ function Projects() {
         </div>
         <div className='mx-8 w-fit order-2 lg:order-1'>
           <span className='text-rose-600'>Web App</span>
-          <h1 className='lg:text-4xl text-2xl my-3'>Fresh Picks Groceries</h1>
+          <h1 className='lg:text-4xl text-2xl my-3'>IBIMA Client Web App</h1>
           <article className='text-slate-200 text-xl leading-loose'>
-            Collaborated with my group of aspiring developers and came up with <br /> Fresh Picks web app for a 
-            grocery company. A web application <br />for displaying products and online shopping
+            Developed a progressive web application for ibima insurance company.<br />This web app helps 
+            their clients purchase insurance covers online <br />and displays a variety of thier products.
           </article>
           <Tooltip label="live demo" color="white" border=" solid 1px red" paddingX={"5px"}>
-            <Link target="_blank" href="https://group-c-vbxo.vercel.app/" className='rounded-full border my-8 border-rose-600 
+            <Link target="_blank" href="https://insurance.ibima.co.ke/" className='rounded-full border my-8 border-rose-600 
               text-rose-600 font-bold hover:bg-rose-600 hover:text-slate-200 w-14 h-14 justify-center flex items-center'>
               <ArrowUpRight />
             </Link>
@@ -73,7 +72,7 @@ function Projects() {
       <CardBody className='lg:flex'>
       <div className='border-2 border-stone-800 p-8 hover:border-rose-600 rounded-md'>
         <Image
-        src="/techverse.png"
+        src="/nobero.png"
         width="500"
         height="500"
         alt="techverse"
@@ -81,13 +80,12 @@ function Projects() {
       </div>
         <div className='mx-8 w-fit'>
         <span className='text-rose-600'>Web App</span>
-          <h1 className='lg:text-4xl text-2xl my-3'>TechVerse</h1>
+          <h1 className='lg:text-4xl text-2xl my-3'>Nobero Eccomerce</h1>
           <article className='text-slate-200 text-xl  leading-loose'>
-            Collaborated with my group of aspiring developers and <br/>came up with TechVerse Web Application.
-            TechVerse<br/> is a software engineering students motivation app.
+            Did a web scrapping project for the nobero eccomerce website.<br/> I used a combination of React in the frontend and Django for the backend.<br/>.
           </article>
           <Tooltip label="live demo" color="white" border=" solid 1px red" paddingX={"5px"}>
-            <Link target="_blank" href="https://techverse-1.onrender.com/" className='rounded-full border my-8 border-rose-600 
+            <Link target="_blank" href="https://noberoclone.netlify.app/" className='rounded-full border my-8 border-rose-600 
            text-rose-600 font-bold hover:bg-rose-600 hover:text-slate-200 w-14 h-14 justify-center flex items-center'>
           <ArrowUpRight />
           </Link>
